fb974fc1630bbce4ab9df7b0e79b03927f6daa40
Nahuel Rodriguez <nahuelr.developer@gmail.com>
2023-05-03 12:30:35 -0300

    Button scroll up

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { hamburgerNav } from "./js/hamburgerNav.js";
+import scrollTopButton from "./js/buttonScroll.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerNav(".hamburger", ".nav", ".nav ul", ".nav ul a", ".hamburger span");
+  scrollTopButton(".button--scroll");
 });
